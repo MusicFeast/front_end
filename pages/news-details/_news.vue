e5aec58ef8ad6b30209c6147d6b6751b0e2bece5
detextre4 <detextre4@gmail.com>
2022-09-24 13:03:42 -0400

    news and fixe animations artists

--- pages/news-details/_news.vue
+++ pages/news-details/_news.vue
@@ -2,6 +2,51 @@
   <div id="news-details">
     <h2 class="Title tup">press &amp; news</h2>
 
+    <section class="container-press-and-news grid">
+      <div class="container-press-and-news--background">
+      </div>
+
+      <article class="divcol gap1">
+        <v-btn icon class="share">
+          <v-icon size="clamp(2em, 2.4vw, 2.4em)">mdi-share-variant</v-icon>
+        </v-btn>
+
+        <h3 class="bold">news title 1</h3>
+        <span class="tcap">title 2</span>
+        <p class="p">
+          Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam 
+          erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo 
+          consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat 
+          nulla facilisis at vero eros et accumsan et iusto odio
+        </p>
+
+        <div class="container-press-and-news--social center gap1">
+          <v-btn v-for="(item,i) in dataSocial" :key="i" icon>
+            <v-icon size="clamp(2em, 2.4vw, 2.4em)">{{item.icon}}</v-icon>
+          </v-btn>
+        </div>
+      </article>
+    </section>
+
+    <blockquote class="divcol">
+      <h2 class="tup" style="text-decoration: underline">lorem ipsum</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam 
+        erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea 
+        commodo consequat.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam 
+        erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea 
+        commodo consequat.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam 
+        erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea 
+        commodo consequat.
+      </p>
+      <span class="marginaleft bold">Lorem ipsum dolor sit amet, </span>
+    </blockquote>
 
     <h2 class="Title tup">other news</h2>
 
@@ -39,6 +84,12 @@ export default {
   name: "NewsDetailsPages",
   data() {
     return {
+      dataSocial: [
+        { icon: "mdi-instagram", link: "#" },
+        { icon: "mdi-twitter", link: "#" },
+        { icon: "mdi-facebook", link: "#" },
+        { icon: "discord", link: "#" },
+      ],
       slider: null,
       dataNews: [
         {
