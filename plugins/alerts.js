d40528b1bcf28f3174a1c7c92a9509703089a855
detextre4 <detextre4@gmail.com>
2022-09-28 11:31:41 -0400

    alerts funciton fixed

--- plugins/alerts.js
+++ plugins/alerts.js
@@ -1,6 +1,6 @@
 export default ({app}, inject) => {
   const params = (key, title, desc) =>
-    app.router.app.$children[app.router.app.$children.findIndex(data=>data._uid === 11)].$refs.alerts.
+    app.router.app.$children[app.router.app.$children.findIndex(data=>data._name === '<DefaultLayout>')].$refs.alerts.
       GenerateAlert(key, title, desc);
   // Inject $alert(key, title, desc) in Vue, context and store.
   inject('alert', params)
