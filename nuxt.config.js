99b7344f9a781d1d49cac98d1d81e6cebf0c2e56
detextre4 <detextre4@gmail.com>
2022-10-13 13:49:33 -0400

    fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -33,7 +33,7 @@ export default {
       // <!-- fonts -->
       { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
       { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' },
-      <!-- inter -->
+      // <!-- inter -->
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap' }
     ],
     script: [
