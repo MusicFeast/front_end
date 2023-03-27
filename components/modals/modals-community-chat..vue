e1054b42df283bf94a0e1e87c6e1d10e3c813d55
detextre4 <detextre4@gmail.com>
2023-03-27 15:59:46 -0400

    modal community and form mf

---
+++ components/modals/modals-community-chat..vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="community-chat-modals">
+    <v-dialog v-model="modalCommunityChat" max-width="500px" content-class="nft-dialog" persistent>
+      <v-btn icon class="close" @click="modalCommunityChat = false">
+        <v-icon large>mdi-close</v-icon>
+      </v-btn>
+
+      <v-window touchless>
+        <v-window-item>
+          <v-card id="modalCommunityChat" class="nft-dialog--content">
+            <!-- <h3>Decline The offer?</h3> -->
+            <p class="p">Return to the <span style="text-decoration: underline" @click="$router.push(localePath('/artists'))">artist</span> page to find the community chat.</p>
+
+            <div class="fwrap gap2 bold" style="margin-top: 2em">
+              <v-btn
+                :ripple="false" class="btn activeBtn" style="--w: min(100%, 12em); --fs: 16px; --bg: #fff; --c: var(--primary)"
+              >Click to verify your membership pass</v-btn>
+              <v-btn
+                :ripple="false" class="btn activeBtn" style="--w: min(100%, 12em); --fs: 16px"
+              >Create Discord account to interact with the platform</v-btn>
+            </div>
+          </v-card>
+        </v-window-item>
+      </v-window>
+    </v-dialog>
+  </div>
+</template>
+
+<script>
+import computeds from '~/mixins/computeds'
+
+export default {
+  name: "ProifleModals",
+  mixins: [computeds],
+  data() {
+    return {
+      modalCommunityChat: false,
+    };
+  },
+  mounted() {
+  },
+  methods: {
+  }
+};
+</script>
+
+<style src="~/assets/styles/components/modals.scss" lang="scss" />
---
+++ pages/form-mf.vue
@@ -0,0 +1,260 @@
+<template>
+  <div id="form-mf" class="divcol">
+    <v-form ref="form" class="grid" @submit.prevent="saveForm()">
+      <h2 class="tup p">Artist Submission</h2>
+
+      <section>
+        <p>
+          Everyone Eats Entertainment would like to first say thank you for your interest in the next step in the evolution of the music industry.
+        </p>
+        <p>
+          Our company is a blend of traditional and non traditional music industry models.  With our technology, you can support your traditional 
+          releases by building an exclusive, unlockable community that allows access to super rare verifiable content and experiences.
+        </p>
+      </section>
+
+      <h2 class="tup p">Artist Submission Form
+        <!-- <sup class="relative" style="top: calc(clamp(15px, 2vw, 30px) * -1)">
+          <v-tooltip
+            right color="rgba(0, 0, 0, .4)" transition="slide-x-transition"
+            :open-delay="100" :close-delay="100">
+            <template #activator="{ on, attrs}">
+              <img src="~/assets/sources/icons/info.svg" alt="info" style="--w: clamp(12px, 1.5vw, 21px)" v-bind="attrs" v-on="on">
+            </template>
+            <span>Texto de ayuda</span>
+          </v-tooltip>
+        </sup> -->
+      </h2>
+
+      <section class="card">
+        <label for="name">Name:</label>
+        <v-text-field
+          id="name"
+          v-model="form.name"
+          placeholder="Mario Perez"
+          :rules="rules.required"
+        ></v-text-field>
+        
+        <label for="artistOrBandName">Artist or Band Name:</label>
+        <v-text-field
+          id="artistOrBandName"
+          v-model="form.artistOrBandName"
+          placeholder="artist or band name"
+          :rules="rules.required"
+        ></v-text-field>
+        
+        <label for="email">Genre:</label>
+        <v-text-field
+          id="genre"
+          v-model="form.genre"
+          placeholder="genre"
+          :rules="rules.required"
+        ></v-text-field>
+        
+        <label for="email">Website::</label>
+        <v-text-field
+          id="website"
+          v-model="form.website"
+          placeholder="website"
+          :rules="rules.required"
+        ></v-text-field>
+        
+        <label for="twitter">Twitter Account</label>
+        <v-text-field
+          id="twitter"
+          v-model="form.twitter"
+          placeholder="@username"
+        ></v-text-field>
+        
+        <label for="instagram">Instagram Account</label>
+        <v-text-field
+          id="instagram"
+          v-model="form.instagram"
+          placeholder="@username#321"
+        ></v-text-field>
+        
+        <label for="facebook">Facebook Account</label>
+        <v-text-field
+          id="facebook"
+          v-model="form.facebook"
+          placeholder="username45"
+        ></v-text-field>
+      </section>
+
+      <section class="terms-and-conditions">
+        <v-checkbox
+          label="By clicking here you acknowledge that building out your community involves your commitment and active participation."
+          :rules="rules.required"
+        />
+        <v-checkbox
+          label="By clicking here you agree to have a member of our team reach out for further discussions."
+          :rules="rules.required"
+        />
+      </section>
+
+      <div class="center fill_w wrap fwrapmobile bold" style="gap:2em; --fb: 200px">
+        <v-btn :ripple="false" class="btn activeBtn" @click="$router.go(-1)">Cancel</v-btn>
+        <v-btn :ripple="false" class="btn activeBtn" @click="saveForm()">Save</v-btn>
+      </div>
+    </v-form>
+  </div>
+</template>
+
+<script>
+import computeds from '~/mixins/computeds'
+
+export default {
+  name: "FormMF",
+  mixins: [computeds],
+  data() {
+    return {
+      userExist: undefined,
+      form: {
+        name: null,
+        artistOrBandName: null,
+        genre: null,
+        website: null,
+        twitter: null,
+        instagram: null,
+        facebook: null,
+        emailAddress: null,
+        phoneNumber: null,
+      },
+      djangoExistenceList: {
+        username: undefined,
+        email: undefined,
+        discord: undefined,
+        instagram: undefined,
+        twitter: undefined,
+        telegram: undefined,
+      },
+      rules: {
+        required: [(v) => !!v || "Field required"],
+        repeatedUsername: [
+          (v) => !!v || "Field required",
+          () => !(this.djangoExistenceList.username) || "Username is already taken"
+        ],
+        repeatedEmail: [
+          (v) => !!v || "Field required",
+          v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+          () => !(this.djangoExistenceList.email) || "Email is already used"
+        ],
+        repeatedDiscord: [
+          () => !(this.djangoExistenceList.discord) || "Discord account is already used"
+        ],
+        repeatedInstagram: [
+          () => !(this.djangoExistenceList.instagram) || "Instagram account is already used"
+        ],
+        repeatedTwitter: [
+          () => !(this.djangoExistenceList.twitter) || "Twitter account is already used"
+        ],
+        repeatedTelegram: [
+          () => !(this.djangoExistenceList.telegram) || "Telegram account is already used"
+        ],
+      },
+    }
+  },
+  head() {
+    const title = "Form MF"
+    return {
+      title,
+    }
+  },
+  // created() {
+  //   this.getData();
+  // },
+  mounted() {
+    this.EnterKeyboardListener();
+  },
+  methods: {
+    async getData() {
+      const accountId = this.$ramper.getAccountId()
+      // get data user
+      await this.$axios.post(`${this.baseUrl}api/v1/get-perfil-data/`, { "wallet": accountId })
+      .then(result => {
+        const data = result.data[0]
+        if (result.data[0]) {
+          this.$equalData(this.form, data)
+          this.form.id = data.id
+          this.imgBanner = data.banner ? this.baseUrl+data.banner : this.user.banner
+          this.imgAvatar = data.avatar ? this.baseUrl+data.avatar : this.user.avatar
+          this.userExist = true
+        } else {
+          this.form.wallet = accountId
+          this.imgBanner = this.user.banner
+          this.imgAvatar = this.user.avatar
+          this.userExist = false
+        }
+      }).catch(err => {
+        this.$alert("cancel", {desc: err.message})
+        console.error(err);
+      })
+    },
+    clearRepeted(key) {
+      if (this.djangoExistenceList[key]) this.djangoExistenceList[key] = undefined
+    },
+    EnterKeyboardListener() {
+      // keyboard enter listener
+      this.$refs.form.$el.querySelectorAll("input").forEach(input => {
+        input.addEventListener("keypress", event => {
+          if (event.key === "Enter") { this.saveForm() }
+        })
+      })
+    },
+    saveForm() {
+      // verification ⭐
+      const consult = {
+        username: this.form.username,
+        email: this.form.email,
+        twitter: this.form.twitter,
+        discord: this.form.discord,
+        instagram: this.form.instagram,
+        telegram: this.form.telegram,
+        wallet: this.form.wallet,
+      }
+      // checkout no repeated info
+      this.$axios.post(`${this.baseUrl}api/v1/validate-perfil/`, consult).then(result => {
+        this.djangoExistenceList = result.data
+        
+        if (!this.$refs.form.validate()) return this.$alert('cancel', {title: 'Failed request', desc: 'Need fill all required fields'})
+        
+        // save form ✔️
+        if (this.userExist) {
+          this.$axios.put(`${this.baseUrl}api/v1/perfil/${this.form.id}/`, this.$formData(this.form))
+          .then(() => this.goBack()).catch(err => {
+            // this.$alert("cancel", {desc: err.message})
+            console.error(err);
+          })
+        } else {
+          this.$axios.post(`${this.baseUrl}api/v1/perfil/`, this.$formData(this.form))
+          .then(() => this.goBack()).catch(err => {
+            this.$alert("cancel", {desc: err.message})
+            console.error(err);
+          })
+        }
+      // catch error repeated values consult
+      }).catch(err => {
+        // this.$alert("cancel", {desc: err.message})
+        console.error(err)
+      })
+    },
+    goBack() {
+      this.$alert({title: "Success!", desc: "Your data has been saved successfully."})
+      setTimeout(() => this.$router.go(0), 400);
+      this.$router.go(-1)
+      
+      // this.$alert("success", {desc: "Your data has been saved successfully."})   
+      // setTimeout(() => this.$alert({title: "Success!", desc: "Your data has been saved successfully."}), 500);
+    },
+    showTag() {document.querySelector(".header").classList.add("hover")},
+    hideTag() {document.querySelector(".header").classList.remove("hover")},
+    previewFile(key, file) {
+      this.form[key] = file;
+      if (key === 'avatar') { this.imgAvatar = URL.createObjectURL(file) }
+      else { this.imgBanner = URL.createObjectURL(file) }
+    },
+  }
+};
+</script>
+
+<style src="~/assets/styles/pages/form-mf.scss" lang="scss" />
