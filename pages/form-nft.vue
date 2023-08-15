f64d33f9ea92dda57eff4e7de122dc5afc68dfa9
sebastian3500 <sebastianrosa39@gmail.com>
2023-08-15 16:41:54 -0400

    commit

--- pages/form-nft.vue
+++ pages/form-nft.vue
@@ -3,14 +3,14 @@
       <h2 class="Title tnone tlow tfirst lines">FORM <span style="text-transform: uppercase!important;">NFT</span></h2>  
       
       <v-row style="margin-top: 40px;">
-        <v-col cols="3" style="position: relative!important;">
+        <v-col xl="2" lg="3" md="3" sm="4" cols="12" style="position: relative!important;">
           <v-file-input class="input-file" prepend-icon="none">
           </v-file-input>
 
           <v-btn class="btn btn-input-file">Upload Image</v-btn>
         </v-col>
 
-        <v-col cols="9">
+        <v-col xl="10" lg="9" md="9" sm="8" cols="12">
           <section class="card">
             <label for="nft-name">NFT Name</label>
             <v-text-field
@@ -52,7 +52,7 @@
         </div>
 
         <v-row class="center">
-          <v-col cols="10">
+          <v-col  xl="10" lg="10" md="10" sm="10" cols="8">
             <label for="nft-name">Song audio</label>
             <v-text-field
               id="song"
@@ -60,13 +60,13 @@
             ></v-text-field>
           </v-col>
 
-          <v-col cols="2">
+          <v-col xl="2" lg="2" md="2" sm="2" cols="4">
             <v-btn class="btn" style="--w: 100%;">Upload</v-btn>
           </v-col>
         </v-row>
 
         <v-row class="center">
-          <v-col cols="10">
+          <v-col  xl="10" lg="10" md="10" sm="10" cols="8">
             <label for="nft-name">Video</label>
             <v-text-field
               id="video"
@@ -74,7 +74,7 @@
             ></v-text-field>
           </v-col>
 
-          <v-col cols="2">
+          <v-col xl="2" lg="2" md="2" sm="2" cols="4">
             <v-btn class="btn" style="--w: 100%;">Upload</v-btn>
           </v-col>
         </v-row>
@@ -92,14 +92,14 @@
         </v-badge>
 
         <v-row class="aend">
-          <v-col cols="9">
+          <v-col xl="9" lg="9" md="9" sm="7" cols="7">
             <label for="near-account">Near Account</label>
             <v-text-field
               id="near-account"
               placeholder="nearaccount.testnet"
             ></v-text-field>
           </v-col>
-          <v-col cols="2">
+          <v-col xl="2" lg="2" md="2" sm="4" cols="4">
             <v-text-field
               placeholder="%"
             ></v-text-field>
@@ -123,14 +123,14 @@
         </v-badge>
 
         <v-row class="aend">
-          <v-col cols="9">
+          <v-col xl="9" lg="9" md="9" sm="7" cols="7">
             <label for="near-account">Near Account</label>
             <v-text-field
               id="near-account"
               placeholder="nearaccount.testnet"
             ></v-text-field>
           </v-col>
-          <v-col cols="2">
+          <v-col xl="2" lg="2" md="2" sm="4" cols="4">
             <v-text-field
               placeholder="%"
             ></v-text-field>
@@ -157,7 +157,26 @@
           <span class="span-badge">Type of merchandise</span>
         </v-badge>
 
-        <hr style="border-color: var(--primary)!important;">
+        <hr class="mb-4" style="border-color: var(--primary)!important;">
+
+        <div class="pa-2 container-card-merchandise">
+          <v-card v-for="(item,index) in dataMerchandise" :key="index" class="card pa-3 card-merchandise">
+            <img :src=item.img alt="img" style="width: 200px;">
+            <div class="div-card-hover">
+              <span class="tcenter" style="text-decoration: underline; font-weight: 700!important;">
+                Merchandise <br> Title
+              </span>
+
+              <span class="tcenter" style="font-size: 13px;">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reprehenderit, eveniet!
+              </span>
+
+              <v-btn class="btn center" style="max-width: 70%!important; min-width: 70%!important;">
+                Select
+              </v-btn>
+            </div>
+          </v-card>
+        </div>
       </section>
 
       <section class="card divrow" style="margin-top: 40px; gap: 40px;">
@@ -174,8 +193,28 @@
 
       <div class="center" style="gap: 10px; margin-top: 40px;">
         <v-btn class="btn" style="--bg: #fff; --c: var(--primary); --fw:700; --w: 150px; --br: 0px;">Cancel</v-btn>
-        <v-btn class="btn" style="--fw:700; --w: 150px; --br: 0px;">Save</v-btn>
+        <v-btn class="btn" style="--fw:700; --w: 150px; --br: 0px;" @click="dialogSuccess = true">Save</v-btn>
       </div>
+
+      <!-- Dialog Success -->
+
+      <v-dialog v-model="dialogSuccess" max-width="500px" content-class="nft-dialog" persistent>
+      <v-btn icon class="close" @click="dialogSuccess = false">
+        <v-icon large>mdi-close</v-icon>
+      </v-btn>
+
+      <v-card id="modalBuy" class="nft-dialog--content quick-help-card">
+        <h2 class="p center" style="--fs: 1.8em; text-decoration: underline;">
+          SUCCESS!
+        </h2>
+        <span>
+          Your transaction was succesful.
+        </span>
+        <v-btn class="btn" style="min-width: 70%!important;" @click="dialogSuccess = false">
+          Ok
+        </v-btn>
+      </v-card>
+    </v-dialog>
     </div>
   </template>
   
@@ -189,6 +228,7 @@
     mixins: [computeds],
     data() {
       return {
+        dialogSuccess: false,
         rules: {
         required: [(v) => !!v || "Field required"],
         repeatedUsername: [
@@ -216,6 +256,21 @@
 
       coming: false,
       visible: false,
+
+      dataMerchandise:[
+        {
+          img: require("~/assets/sources/images/shirt.png")
+        },
+        {
+          img: require("~/assets/sources/images/sweatter.png")
+        },
+        {
+          img: require("~/assets/sources/images/cap.png")
+        },
+        {
+          img: require("~/assets/sources/images/cup.png")
+        },
+      ]
       }
     },
     head() {
@@ -230,5 +285,5 @@
   };
   </script>
   
-  <style src="~/assets/styles/pages/form.scss" lang="scss" />
+  <style src="~/assets/styles/pages/form-nft.scss" lang="scss" />
   
