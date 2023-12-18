7555b1d188ef4adfa642d274621d70a4aca9fcb4
sebastian3500 <sebastianrosa39@gmail.com>
2023-12-18 13:41:20 -0400

    commit

--- pages/edit-profile.vue
+++ pages/edit-profile.vue
@@ -442,97 +442,139 @@
       :class="{ ownCollection: true }"
     >
       <template v-for="(item, i) in dataCollections_pagination">
-        <v-card
-          v-if="!item.state"
+        <v-sheet
           :key="i"
-          class="card divcol custome"
-          @click="$store.dispatch('goTo', { key: 'nft', item, event: $event })"
+          color="rgba(0, 0, 0, .4)"
+          class="divcol"
         >
-          <v-img
-            :src="item.img"
-            :alt="`${item.name} image`"
-            transition="fade-transition"
-            :style="`${item.state ? `--tag-state: '${item.state}'` : ''}`"
+          <v-card
+            v-if="!item.state"
+            :key="i"
+            class="card divcol custome"
+            @click="$store.dispatch('goTo', { key: 'nft', item, event: $event })"
           >
-            <template #placeholder>
-              <v-skeleton-loader type="card" />
-            </template>
-          </v-img>
-
-          <!-- TODO put location to router or whatever logic needed here -->
-          <!-- <v-btn :ripple="false" class="btn activeBtn editBtn"
-            >Edit Profile</v-btn
-          > -->
-
-          <div class="container-content tcenter">
-            <v-avatar style="border: 2px solid #fff">
-              <v-img
-                :src="item.avatar"
-                :alt="`${item.artist} image`"
-                transition="fade-transition"
-              >
-                <template #placeholder>
-                  <v-skeleton-loader type="avatar" />
-                </template>
-              </v-img>
-            </v-avatar>
-            <a>{{ item.name }}</a>
-            <p v-html="item.desc"></p>
-
-            <div class="center bold" style="gap: 6.4px">
-              <span class="floor" style="--c: var(--accent)"
-                >Price: {{ item.price }} $</span
-              >
-              <!-- <img src="@/assets/sources/logos/near-orange.svg" alt="near" style="--w:15px"> -->
-            </div>
-            <span class="floor" style="--c: var(--accent)"
-              >Editions: {{ item.editions }}</span
+            <!-- <v-img
+              :src="item.img"
+              :alt="`${item.name} image`"
+              transition="fade-transition"
+              :style="`${item.state ? `--tag-state: '${item.state}'` : ''}`"
             >
-          </div>
-        </v-card>
-        <v-card v-else :key="i" class="card divcol custome">
-          <v-img
-            :src="item.img"
-            :alt="`${item.name} image`"
-            transition="fade-transition"
-            :style="`${item.state ? `--tag-state: '${item.state}'` : ''}`"
-          >
-            <template #placeholder>
-              <v-skeleton-loader type="card" />
-            </template>
-          </v-img>
+              <template #placeholder>
+                <v-skeleton-loader type="card" />
+              </template>
+            </v-img> -->
 
-          <!-- TODO put location to router or whatever logic needed here -->
-          <v-btn :ripple="false" class="btn activeBtn editBtn">
-            <v-icon>mdi-pencil</v-icon>
-          </v-btn>
-
-          <div class="container-content tcenter">
-            <v-avatar style="border: 2px solid #fff">
-              <v-img
-                :src="item.avatar"
-                :alt="`${item.artist} image`"
-                transition="fade-transition"
-              >
-                <template #placeholder>
-                  <v-skeleton-loader type="avatar" />
-                </template>
-              </v-img>
-            </v-avatar>
-            <a>{{ item.name }}</a>
-            <p v-html="item.desc"></p>
-
-            <div class="center bold" style="gap: 6.4px">
-              <span class="floor" style="--c: var(--accent)"
-                >Price: {{ item.price }} $</span
-              >
-              <!-- <img src="@/assets/sources/logos/near-orange.svg" alt="near" style="--w:15px"> -->
-            </div>
-            <span class="floor" style="--c: var(--accent)"
-              >Editions: {{ item.editions }}</span
+            <v-img
+              :src="item.img"
+              :alt="`${item.name} image`"
+              transition="fade-transition"
             >
+              <template #placeholder>
+                <v-skeleton-loader type="card" />
+              </template>
+              <v-btn
+                :disabled="item.tier != 1 && item.tier != 2"
+                class="btn"
+                style="position: absolute !important; right: 5px; top: 5px"
+                @click="goToForm(item)"
+                >Edit this Tier</v-btn
+              >
+            </v-img>
+
+            <!-- TODO put location to router or whatever logic needed here -->
+            <!-- <v-btn :ripple="false" class="btn activeBtn editBtn"
+              >Edit Profile</v-btn
+            > -->
+
+            <div class="container-content tcenter">
+              <v-avatar style="border: 2px solid #fff">
+                <v-img
+                  :src="item.avatar"
+                  :alt="`${item.artist} image`"
+                  transition="fade-transition"
+                >
+                  <template #placeholder>
+                    <v-skeleton-loader type="avatar" />
+                  </template>
+                </v-img>
+              </v-avatar>
+              <a>{{ item.name }}</a>
+              <p v-html="item.desc"></p>
+
+              <div class="center bold" style="gap: 6.4px">
+                <span class="floor" style="--c: var(--accent)"
+                  >Price: {{ item.price }} $</span
+                >
+                <!-- <img src="@/assets/sources/logos/near-orange.svg" alt="near" style="--w:15px"> -->
+              </div>
+              <span class="floor" style="--c: var(--accent)"
+                >Editions: {{ item.editions }}</span
+              >
+            </div>
+          </v-card>
+          <v-card v-else :key="i" class="card divcol custome">
+            <v-img
+              :src="item.img"
+              :alt="`${item.name} image`"
+              transition="fade-transition"
+              :style="`${item.state ? `--tag-state: '${item.state}'` : ''}`"
+            >
+              <template #placeholder>
+                <v-skeleton-loader type="card" />
+              </template>
+            </v-img>
+
+            <!-- TODO put location to router or whatever logic needed here -->
+            <v-btn :ripple="false" class="btn activeBtn editBtn">
+              <v-icon>mdi-pencil</v-icon>
+            </v-btn>
+
+            <div class="container-content tcenter">
+              <v-avatar style="border: 2px solid #fff">
+                <v-img
+                  :src="item.avatar"
+                  :alt="`${item.artist} image`"
+                  transition="fade-transition"
+                >
+                  <template #placeholder>
+                    <v-skeleton-loader type="avatar" />
+                  </template>
+                </v-img>
+              </v-avatar>
+              <a>{{ item.name }}</a>
+              <p v-html="item.desc"></p>
+
+              <div class="center bold" style="gap: 6.4px">
+                <span class="floor" style="--c: var(--accent)"
+                  >Price: {{ item.price }} $</span
+                >
+                <!-- <img src="@/assets/sources/logos/near-orange.svg" alt="near" style="--w:15px"> -->
+              </div>
+              <span class="floor" style="--c: var(--accent)"
+                >Editions: {{ item.editions }}</span
+              >
+            </div>
+          </v-card>
+
+          <div class="tier-desc divcol">
+            <a class="tup bold" style="cursor: default">{{ item.name }}</a>
+            <ul>
+              <li
+                v-show="item.desc"
+                v-html="item.desc.limitString(110)"
+              ></li>
+            </ul>
           </div>
-        </v-card>
+
+          <div class="container-actions divcol">
+            <a>More Details</a>
+            <v-btn
+              :ripple="false"
+              class="btn activeBtn align"
+              style="--w: calc(100% - 1em)"
+              >Go to Buy page</v-btn>
+          </div>
+        </v-sheet>
       </template>
     </section>
 
